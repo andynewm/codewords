b5063a50bc85c0b58c0b1d974c8c69f32c3f04a4
Andy Newman <anxx88@gmail.com>
2014-09-07 18:29:32 +0100

    Unprototyping the Puzzle

--- app/scripts/services/puzzle.js
+++ app/scripts/services/puzzle.js
@@ -9,7 +9,7 @@ angular.module('codeword')
 
 			$http.get('puzzles/puzzle' + index + '.json')
 				.success(function (data) {
-					deferred.resolve(new Puzzle(data, index));
+					deferred.resolve(Puzzle(data, index));
 				})
 				.error(function () {
 					deferred.reject();
@@ -19,129 +19,118 @@ angular.module('codeword')
 		}
 
 		function Puzzle(puzzle, number) {
-			this.state = storage.getState(number) || puzzle.state;
-			this.number = number;
-			this.initState = puzzle.state.slice(0);
-			this.solution = puzzle.solution;
-			this.map = puzzle.map;
-			this.inverseState = this.state.reduce(function (obj, item, index) {
-				if (item) {
-					obj[item] = index + 1;
-				}
-				return obj;
-			}, {});
-			this.inverseInitialState = this.initState.reduce(function (obj, item, index) {
-				if (item) {
-					obj[item] = index + 1;
-				}
-				return obj;
-			}, {});
-			this.undoStack = [];
-			this.redoStack = [];
-		}
+			var state = storage.getState(number) || puzzle.state,
+			    initState = puzzle.state.slice(0),
+			    solution = puzzle.solution,
+			    map = puzzle.map,
+			    inverseState = getInverseState(state),
+			    inverseInitialState = getInverseState(initState),
+			    undoStack = [],
+			    redoStack = [];
 
-		Puzzle.prototype.setLetter = function (code, letter) {
-			if (this.inverseInitialState[letter]) {
-				return;
+			function getInverseState(forwardState) {
+				return forwardState.reduce(function (obj, item, index) {
+					if (item) {
+							obj[item] = index + 1;
+						}
+						return obj;
+					}, {});
 			}
 
-			var state = this.state,
-			    inverseState = this.inverseState
-			    undoStack = this.undoStack
-			    redoStack = this.redoStack;
-
-			undoStack.push(state.slice(0));
-			redoStack.length = 0;
-
-			if (state[code]) {
-				delete inverseState[state[code]];
-			}
-
-			state.forEach(function (value, i) {
-				if (value == letter) {
-					state[i] = null;
-				}
-			});
-
-			state[code] = letter;
-
-			inverseState[letter] = code + 1;
-
-			storage.saveState(this.number, this.state);
-
-			if (this.isSolved()) {
-				storage.setGlobalState(this.number, 'solved');
-			}
-			else {
-				storage.setGlobalState(this.number, 'inProgress');
-			}
-		};
-
-		Puzzle.prototype.undo = function () {
-			var state= this.state,
-			    inverseState = this.inverseState,
-			    undoStack = this.undoStack,
-			    redoStack = this.redoStack;
-
-			if (undoStack.length) {
-				redoStack.push(state.slice(0));
-
-				for(prop in inverseState) {
-					delete inverseState[prop];
+			function setLetter(code, letter) {
+				if (inverseInitialState[letter]) {
+					return;
 				}
 
-				undoStack.pop().forEach(function (letter, i) {
-					if (letter) {
-						inverseState[letter] = i + 1;
-					}
-					state[i] = letter;
-				});
-			}
-		}
-
-		Puzzle.prototype.redo = function () {
-			var state= this.state,
-			    inverseState = this.inverseState,
-			    undoStack = this.undoStack,
-			    redoStack = this.redoStack;
-
-			if (redoStack.length) {
 				undoStack.push(state.slice(0));
+				redoStack.length = 0;
 
-				for(prop in inverseState) {
-					delete inverseState[prop];
+				if (state[code]) {
+					delete inverseState[state[code]];
 				}
 
-				redoStack.pop().forEach(function (letter, i) {
-					if (letter) {
-						inverseState[letter] = i + 1;
+				state.forEach(function (value, i) {
+					if (value == letter) {
+						state[i] = null;
 					}
-					state[i] = letter;
 				});
+
+				state[code] = letter;
+
+				inverseState[letter] = code + 1;
+
+				storage.saveState(number, state);
+
+				if (isSolved()) {
+					storage.setGlobalState(number, 'solved');
+				}
+				else {
+					storage.setGlobalState(number, 'inProgress');
+				}
 			}
+
+			function undo() {
+				if (undoStack.length) {
+					redoStack.push(state.slice(0));
+
+					for(prop in inverseState) {
+						delete inverseState[prop];
+					}
+
+					undoStack.pop().forEach(function (letter, i) {
+						if (letter) {
+							inverseState[letter] = i + 1;
+						}
+						state[i] = letter;
+					});
+				}
+			}
+
+			function redo() {
+				if (redoStack.length) {
+					undoStack.push(state.slice(0));
+
+					for(prop in inverseState) {
+						delete inverseState[prop];
+					}
+
+					redoStack.pop().forEach(function (letter, i) {
+						if (letter) {
+							inverseState[letter] = i + 1;
+						}
+						state[i] = letter;
+					});
+				}
+			}
+
+			function isSolved() {
+				return state.every(function (x, i) {
+					return x == solution[i];
+				});
+			};
+
+			function isValid() {
+				return state.every(function (x, i) {
+					return !x || x == solution[i];
+				})
+			}
+
+			function match(letter) {
+				return inverseState[letter];
+			};
+
+			return {
+				state: state,
+				map: map,
+				initState: initState,
+				setLetter: setLetter,
+				undo: undo,
+				redo: redo,
+				isSolved: isSolved,
+				isValid: isValid,
+				match: match
+			};
 		}
-
-		Puzzle.prototype.isSolved = function () {
-			var state = this.state,
-			    solution = this.solution;
-
-			return state.every(function (x, i) {
-				return x == solution[i];
-			});
-		};
-
-		Puzzle.prototype.isValid = function () {
-			var state = this.state,
-			    solution = this.solution;
-
-			return state.every(function (x, i) {
-				return !x || x == solution[i];
-			})
-		}
-
-		Puzzle.prototype.match = function (letter) {
-			return this.inverseState[letter];
-		};
 
 		return { load: load };
 
